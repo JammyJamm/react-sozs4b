8b82c74ca51de09952fe2614e3d7c89c8797d1e3
JammyJamm <tamiltanish@gmail.com>
2024-01-05 15:43:22 +0530

    #Removed Learning changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import "./App.css";
 import React from "react";
 import DataLayout from "./project/DataLayout";
-import Learning from "./learning/Learning";
 // import "bootstrap/dist/css/bootstrap.min.css";
 // import "jquery/dist/jquery.min.js";
 // import "bootstrap/dist/js/bootstrap.min.js";
