d2b8c46ada3549309d8bf1549e50bbcc24e55b18
JammyJamm <tamiltanish@gmail.com>
2024-01-08 09:46:44 +0530

    #PlayerData - WORKS

--- src/players/AddPlayer.js
+++ src/players/AddPlayer.js
@@ -1,54 +1,131 @@
-import { useState } from "react"
 
-const AddPlayer = ()=>{
-    const [league,setLeague] = useState({
-        league:"BBl",
-        player:"Maxwell",
-        position:4,
-        state: "All Rounder",
-        captan:true,
-        score: 24,
-       result:"Win",
+import { useState } from "react";
+import db from "../FirbaseConfig";
+import { addDoc, collection } from "@firebase/firestore";
+
+const AddPlayer = () => {
+  const [data, setData] = useState("");
+  const [form, setForm] = useState([
+    {
+      match: "",
+      matchData: {
+        gender: "", 
+        genderData: {
+          league: "",
+          leagueData: {
+            player: "",
+            captain: "",
+            position: "",
+            about: "",
+            score: "",
+            result: "",
+          },
+        },
+      },
+    },
+  ]);
+  const handlePlayerScore = (e) => {
+    e.preventDefault();
+    // validationPlayer();
+    const addForm = document.querySelector("#addPlayer");
+    const colRef = collection(db, "livematch");
+    addDoc(colRef, {
+      league: addForm.league.value,
+      gender: addForm.gender.value,
+      match: addForm.match.value,
+      player: addForm.player.value,
+      captain: addForm.captain.value,
+      position: addForm.position.value,
+      about: addForm.about.value,
+      score: addForm.score.value,
+      result: addForm.result.value,
     })
-    const addPlayerForm = ()=>{
-        console.log("form")
-    }
-    return(
-        <div className="ui-playerform">
-           <form onSubmit={addPlayerForm}>
-           <div class="ui-form-element">
-            <select name="league">
-                <option>Data</option>
-            </select>
-            </div>
-            <div class="ui-form-element">
-            <input type="text" name="player"></input>
-            </div>
-            <div class="ui-form-element">
-            <select name="state">
-                <option value={"Wk"}>Wicket Keeper</option>
-                <option value={"Bat"}>Batter</option>
-                <option value={"All"}>All Rounder</option>
-                <option value={"Bowl"}>Bowler</option>
-            </select>
-            </div>
-            <div class="ui-form-element">
-            <select name="captan">
-                <option value="yes" >Yes</option>
-                <option value="no" >No</option>
-            </select>  
-            </div>
-            <div class="ui-form-element"> 
-            <input type="text" name="score"></input>
-            </div>
-            <div class="ui-form-element">
-            <select name="result">
-                <option value="win" >Win</option>
-                <option value="loss" >loss</option>
-            </select>  
-            </div>
-           </form>
+      .then((response) => {
+        alert("Added");
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+    // console.log(form);
+  };
+  //  function validationPlayer(){
+  //     return
+  //  }
+  const playerChange = (e) => {
+    return e.target.value;
+  };
+  return (
+    <div className="ui-form card col-12">
+      <form id="addPlayer" onSubmit={handlePlayerScore}>
+        <div className="form-elements col-4 ">
+          <label>Match</label>
+          <select name="match" onChange={playerChange}>
+            <option value="T20">T20</option>
+            <option value="ODI">ODI</option>
+          </select>
         </div>
-    )
-}
+        <div className="form-elements col-4">
+          <label>Gender</label>
+          <select name="gender" onChange={playerChange}>
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+          </select>
+        </div>
+        <div className="form-elements col-4">
+          <label>League</label>
+          <select name="league" onChange={playerChange}>
+            <option value="BBL">BBl League</option>
+            <option value="InterNational">International</option>
+            <option value="IPL">IPL</option>
+          </select>
+        </div>
+        <div className="form-elements col-4">
+          <label>Player Name</label>
+          <input type="text" name="player" onChange={playerChange} />
+        </div>
+        <div className="form-elements col-4">
+          <label>Captain</label>
+          <select name="captain" onChange={playerChange}>
+            <option value="yes">Yes</option>
+            <option value="no">No</option>
+          </select>
+        </div>
+        <div className="form-elements col-4">
+          <label>Batting position</label>
+          <select name="position" onChange={playerChange}>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
+          </select>
+        </div>
+        <div className="form-elements col-4">
+          <label>About</label>
+          <select name="about" onChange={playerChange}>
+            <option value="Wk">Wicket Keeper</option>
+            <option value="Bat">Battter</option>
+            <option value="All">All Rounder</option>
+          </select>
+        </div>
+        <div className="form-elements col-4">
+          <label>Score</label>
+          <input type="number" name="score" onChange={playerChange} />
+        </div>
+        <div className="form-elements col-4">
+          <label>Result</label>
+          <select name="result" onChange={playerChange}>
+            <option value="Win">Win</option>
+            <option value="Loss">Loss</option>
+          </select>
+        </div>
+        <button className="blue-btn">Submit</button>
+      </form>
+    </div>
+  );
+};
 export default AddPlayer;
+
--- src/players/PlayerData.js
+++ src/players/PlayerData.js
@@ -1,33 +1,102 @@
-const PlayerData  = ()=>{
-    const shoot = () => {
-        console.log("Senthmail")
-      }
-    return (
-        <div className="PayerData">
-            <button onClick={shoot}>BBL</button>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Player Name</th>
-                        <th>Position </th>
-                        <th>Details </th>
-                        <th>Score</th>
-                        <th>Result</th> 
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>
-                            Maxwell
-                        </td>
-                        <td>2</td>
-                        <td>All Rouder</td>
-                        <td>28</td>
-                        <td></td>   
-                    </tr>
-                </tbody>
-            </table>
+import { useEffect, useState } from "react";
+import AddPlayer from "./AddPlayer";
+import { collection, getDocs, query } from "@firebase/firestore";
+import db from "../FirbaseConfig";
+const PlayerData = () => {
+    const [playerData,setPlayerData] = useState("")
+  const [detail, setDetail] = useState([]);
+  const userData = async () => {
+    // document.getElementById("loaderSpinner").style.display = "flex";
+    // Get selected Value data
+    const q = query(collection(db, "livematch"));
+    const querySnapshot = await getDocs(q);
+    const data = querySnapshot.docs.map((doc) => ({
+      ...doc.data(),
+      id: doc.id,
+      //
+      ...doc.data(),
+    }));
+    setDetail(data);
+    handleFilter(data);
+   
+    // document.getElementById("loaderSpinner").style.display = "none";
+  };
+  useEffect(() => {
+    userData();
+  }, []);
+
+  /// userData
+  const handleFilter = (data) => {
+    const addForm = document.querySelector("#playerFilter");
+    const selectMatch = addForm.match;
+    const selectGender = addForm.gender;
+    const selectLeague = addForm.league;
+    const ds = detail.filter(
+      ({ match, league, gender }) =>
+        match === selectMatch.value &&
+         league === selectLeague.value &&
+         gender === selectGender.value
+    );
+    setPlayerData(ds)
+  };
+
+  return (
+    <div className="ui-player">
+      <form id="playerFilter" className="ui-form card col-12">
+        <div className="form-elements col-4 ">
+          <label>Match</label>
+          <select name="match" onChange={handleFilter}>
+            <option value="T20">T20</option>
+            <option value="ODI">ODI</option>
+          </select>
         </div>
-    )
-}
+        <div className="form-elements col-4">
+          <label>Gender</label>
+          <select name="gender" onChange={handleFilter}>
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+          </select>
+        </div>
+        <div className="form-elements col-4">
+          <label>League</label>
+          <select name="league" onChange={handleFilter}>
+            <option value="BBL">BBl League</option>
+            <option value="InterNational">International</option>
+            <option value="IPL">IPL</option>
+          </select>
+        </div>
+      </form>
+      <div className="card col-12">
+        <table>
+          <thead>
+            <tr>
+              <th>Player Name</th>
+              <th>Batting position</th>
+              <th>Role</th>
+              <th>Captain</th>
+              <th>Score</th>
+              <th>Win</th>
+            </tr>
+          </thead>
+          <tbody>
+            {playerData.map((list) => {
+              return (
+                <tr>
+                  <td>{list.player}</td>
+                  <td>{list.position}</td>
+                  <td>{list.about}</td>
+                  <td>{list.captain}</td>
+                  <td>{list.score}</td>
+                  <td>{list.result}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+      <AddPlayer />
+    </div>
+  );
+};
 export default PlayerData;
+
