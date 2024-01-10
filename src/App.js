cf9287ccc81907a50518e324c12bd3d72315c41d
JammyJamm <59868544+JammyJamm@users.noreply.github.com>
2024-01-10 15:18:56 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import "./App.css";
-import React from "react";
-import DataLayout from "./project/DataLayout";
-import Nav from "./Nav";
+import './App.css';
+import React from 'react';
+import DataLayout from './project/DataLayout';
+
 // import "bootstrap/dist/css/bootstrap.min.css";
 // import "jquery/dist/jquery.min.js";
 // import "bootstrap/dist/js/bootstrap.min.js";
@@ -9,9 +9,9 @@ function App() {
   return (
     <div className="ui-app-layout container-fluid">
       <div className="row">
-        <Nav />
+        <DataLayout />
       </div>
     </div>
   );
 }
-export default App; 
+export default App;
